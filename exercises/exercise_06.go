3fa50569ab15086fd1b8fe8cdf69c7cbebb6508d
Goblin Teacher <gogoblin.dev@gmail.com>
2025-04-28 23:07:18 +0300

    first commit

---
+++ exercises/exercise_06.go
@@ -0,0 +1,26 @@
+package exercises
+
+import (
+	"context"
+	"time"
+)
+
+// 6. Остановка горутины через context.WithTimeout
+// Задача:
+// Запустите горутину, которая каждую секунду выводит `"tick"`.
+// Используйте `context.WithTimeout`, чтобы горутина завершилась автоматически через 3 секунды.
+
+func worker(ctx context.Context) {
+	for {
+		// TODO ЛОГИКА
+	}
+}
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	go worker(ctx)
+
+	time.Sleep(4 * time.Second)
+}
